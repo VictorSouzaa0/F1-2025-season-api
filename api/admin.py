70deb13edc816ca239333fa5799f91aec1227f6f
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2025-03-14 06:31:18 -0300

    create legend

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Driver
+from .models import Driver,Legend
 
 
 admin.site.register(Driver)
+admin.site.register(Legend)
--- api/models.py
+++ api/models.py
@@ -3,10 +3,21 @@ from django.db import models
 
 class Driver(models.Model):
     image = models.ImageField()
-    name =  models.CharField(max_length=255)
+    name =  models.CharField(max_length=255, primary_key=True)
     birth_date = models.DateField()
     team = models.CharField(max_length=255)
     country = models.CharField(max_length=255)
 
     def __str__(self):
         return f'Driver: {self.name} | {self.team}'
+    
+class Legend(models.Model):
+    image = models.ImageField()
+    name = models.CharField(max_length=255, primary_key=True)
+    birth_date = models.DateField()
+    country = models.CharField(max_length=100)
+    championships = models.IntegerField()
+    teams = models.TextField()
+
+    def __str__(self):
+        return f"Driver: {self.name} | {self.country}"
